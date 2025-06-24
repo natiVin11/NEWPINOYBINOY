153b317caaf60012dbf1c5108546d39604eefc36
natiVin11 <77802978+natiVin11@users.noreply.github.com>
2025-06-24 21:49:46 +0300

    Update server.js

--- server.js
+++ server.js
@@ -414,4 +414,5 @@ app.get('/user-buildings/:userId', (req, res) => {
 
 
 
-app.listen(3000, () => console.log('Server running on http://localhost:3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
